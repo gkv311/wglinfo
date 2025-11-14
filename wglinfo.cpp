cbbfcc628d4fd0b6b2dd0197092364b44a1187c7
Kirill Gavrilov <kirill@sview.ru>
2025-11-14 08:22:39 +0300

    Extend command-line syntax with options --platform {wgl|egl} --api {gl|gles}
    --profile {compat|core|soft}

--- wglinfo.cpp
+++ wglinfo.cpp
@@ -10,127 +10,311 @@
 #include "EglGlContext.h"
 #include "NativeGlContext.h"
 
-#include <string>
 #include <iomanip>
 #include <iostream>
+#include <string>
+#include <vector>
 
-static int actual_main(int theNbArgs, const char** theArgVec)
+//! Information tool.
+class WglInfo
 {
-  bool isVerbose = false, toPrintVisuals = true, toShowEgl = true;
-  bool toShowWgl = true;
+public:
+  //! Perform tool.
+  int Perform(int theNbArgs, const char** theArgVec);
+
+private:
+
+  //! Parse arguments.
+  bool parseArguments(int theNbArgs, const char** theArgVec);
+
+  //! Print help message.
+  void printHelp(const char* theName);
+
+  //! Print WGL info.
+  template<class Platform_t>
+  std::vector<BaseGlContext::ContextBits> printWglInfo();
+
+  //! Returns the CPU architecture used to build the program (may not match the system).
+  static const char* getArchString();
+
+  //! Print build and system information.
+  static void printSystemInfo();
+
+private:
+  bool myToShowEgl = true;
+  bool myToShowWgl = true;
+
+  bool myToShowGl = true;
+  bool myToShowGles = true;
+
+  bool myIsCompatProfile = true;
+  bool myIsCoreProfile = true;
+  bool myIsSoftProfile = true;
+
+  bool myIsVerbose = false;
+  bool myToPrintVisuals = true;
+
+  int myExitCode = 1;
+};
+
+int main(int argc, const char** argv)
+{
+  WglInfo aTool;
+  return aTool.Perform(argc, argv);
+}
+
+int WglInfo::Perform(int theNbArgs, const char** theArgVec)
+{
+  printSystemInfo();
+
+  myExitCode = 0;
+  if (!parseArguments(theNbArgs, theArgVec))
+    return myExitCode;
+
+  const std::vector<BaseGlContext::ContextBits> aWglDone =
+    myToShowWgl ? printWglInfo<NativeGlContext>() : std::vector<BaseGlContext::ContextBits>();
+  const std::vector<BaseGlContext::ContextBits> aEglDone =
+    myToShowEgl ? printWglInfo<EglGlContext>() : std::vector<BaseGlContext::ContextBits>();
+  if (aWglDone.empty() && aEglDone.empty())
+    myExitCode = 1;
+
+  if (myToPrintVisuals)
+  {
+    if (!aWglDone.empty())
+    {
+      NativeGlContext aDummy("wglinfo_dummy");
+      if (aDummy.CreateGlContext(aWglDone[0]))
+        aDummy.PrintVisuals(myIsVerbose);
+    }
+    if (!aEglDone.empty())
+    {
+      EglGlContext aDummy("wglinfo_dummy");
+      if (aDummy.CreateGlContext(aEglDone[0]))
+        aDummy.PrintVisuals(myIsVerbose);
+    }
+  }
+
+  return myExitCode;
+}
+
+//! Convert string to lower case.
+static std::string stringToLowerCase(std::string theStr)
+{
+  for (size_t i = 0; i < theStr.length(); ++i)
+    theStr[i] = (char)tolower(theStr[i]);
+
+  return theStr;
+}
+
+bool WglInfo::parseArguments(int theNbArgs, const char** theArgVec)
+{
   for (int anArgIter = 1; anArgIter < theNbArgs; ++anArgIter)
   {
-    if (memcmp(theArgVec[anArgIter], "-v", 2) == 0)
+    const std::string anArg = stringToLowerCase(theArgVec[anArgIter]);
+    if (anArg == "-v")
     {
-      isVerbose = true;
+      myIsVerbose = true;
     }
-    else if (memcmp(theArgVec[anArgIter], "-B", 2) == 0)
+    else if (anArg == "-b")
     {
-      toPrintVisuals = false;
+      myToPrintVisuals = false;
     }
-    else if (memcmp(theArgVec[anArgIter], "-h", 2) == 0
-          || memcmp(theArgVec[anArgIter], "--help", 6) == 0
-          || memcmp(theArgVec[anArgIter], "/?", 2) == 0)
+    else if ((anArg == "--platform" || anArg == "-platform")
+              && anArgIter + 1 < theNbArgs)
     {
-      std::cout << "Usage: " << theArgVec[0] << "\n"
-                << "  -B: brief output, print only the basics.\n"
-	                 "  -v: Print visuals info in verbose form.\n"
-	                 "  -h: This information\n"
-                << "This wglinfo tool variation has been created by Kirill Gavrilov Tartynskih <kirill@sview.ru>\n";
-      return 0;
+      const std::string aVal = stringToLowerCase(theArgVec[++anArgIter]);
+      myToShowWgl = myToShowEgl = false;
+      if (aVal == "*")
+      {
+        myToShowWgl = myToShowEgl = true;
+      }
+      else if (aVal == "egl")
+      {
+        myToShowEgl = true;
+      }
+      else if (aVal == "wgl")
+      {
+        myToShowWgl = true;
+      }
+      else
+      {
+        std::cerr << "Syntax error! Unknown platform '" << theArgVec[anArgIter] << "'\n\n";
+        return false;
+      }
+    }
+    else if (anArg == "egl")
+    {
+      myToShowEgl = true;
+      myToShowWgl = false;
+    }
+    else if (anArg == "wgl")
+    {
+      myToShowEgl = false;
+      myToShowWgl = true;
+    }
+    else if ((anArg == "--api" || anArg == "-api")
+              && anArgIter + 1 < theNbArgs)
+    {
+      const std::string aVal = stringToLowerCase(theArgVec[++anArgIter]);
+      myToShowGl = myToShowGles = false;
+      if (aVal == "*")
+      {
+        myToShowGl = myToShowGles = true;
+      }
+      else if (aVal == "gl" || aVal == "opengl")
+      {
+        myToShowGl = true;
+      }
+      else if (aVal == "gles" || aVal == "opengles" || aVal == "opengl_es" || aVal == "opengl es")
+      {
+        myToShowGles = true;
+      }
+      else
+      {
+        std::cerr << "Syntax error! Unknown api '" << theArgVec[anArgIter] << "'\n\n";
+        return false;
+      }
+    }
+    else if (anArg == "gl" || anArg == "opengl")
+    {
+      myToShowGl = true;
+      myToShowGles = false;
+    }
+    else if (anArg == "gles" || anArg == "opengles" || anArg == "opengl_es" || anArg == "opengl es")
+    {
+      myToShowGl = false;
+      myToShowGles = true;
+    }
+    else if ((anArg == "--profile" || anArg == "-profile")
+           && anArgIter + 1 < theNbArgs)
+    {
+      const std::string aVal = stringToLowerCase(theArgVec[++anArgIter]);
+      if (aVal != "*")
+      {
+        myToShowGl = true;
+        myToShowGles = false;
+      }
+      myIsCompatProfile = myIsCoreProfile = myIsSoftProfile = false;
+      if (aVal == "*")
+      {
+        myIsCompatProfile = myIsCoreProfile = myIsSoftProfile = true;
+      }
+      else if (aVal == "compat" || aVal == "compatible"
+            || aVal == "compatible_profile" || aVal == "compatible profile")
+      {
+        myIsCompatProfile = true;
+      }
+      else if (aVal == "core" || aVal == "core_profile" || aVal == "core profile")
+      {
+        myIsCoreProfile = true;
+      }
+      else if (aVal == "soft" || aVal == "noacc" || aVal == "no_acceleration")
+      {
+        myIsSoftProfile = true;
+      }
+      else
+      {
+        std::cerr << "Syntax error! Unknown profile '" << theArgVec[anArgIter] << "'\n\n";
+        return false;
+      }
+    }
+    else if (anArg == "compat" || anArg == "compatible"
+          || anArg == "compatible_profile" || anArg == "compatible profile")
+    {
+      myToShowGl = true;
+      myToShowGles = false;
+      myIsCompatProfile = true;
+      myIsCoreProfile = false;
+      myIsSoftProfile = false;
+    }
+    else if (anArg == "core" || anArg == "core_profile" || anArg == "core profile")
+    {
+      myToShowGl = true;
+      myToShowGles = false;
+      myIsCompatProfile = false;
+      myIsCoreProfile = true;
+      myIsSoftProfile = false;
+    }
+    else if (anArg == "soft" || anArg == "noacc" || anArg == "no_acceleration")
+    {
+      myToShowGl = true;
+      myToShowGles = false;
+      myIsCompatProfile = false;
+      myIsCoreProfile = false;
+      myIsSoftProfile = true;
+    }
+    else if (anArg == "-h" || anArg == "--help" || anArg == "/?")
+    {
+      printHelp(theArgVec[0]);
+      return false;
     }
     else
     {
       std::cerr << "Syntax error! Unknown argument '" << theArgVec[anArgIter] << "'\n\n";
-      const char* anArgs[2] = { theArgVec[0], "-h" };
-      actual_main(2, anArgs);
-      return 1;
+      printHelp(theArgVec[0]);
+      myExitCode = 1;
+      return false;
     }
   }
-
-  if (toShowWgl)
-  {
-    NativeGlContext aCtxCompat("wglinfo");
-    if (aCtxCompat.CreateGlContext(NativeGlContext::ContextBits_NONE))
-    {
-      // print WGL info
-      aCtxCompat.PrintPlatformInfo();
-      aCtxCompat.PrintRendererInfo();
-      aCtxCompat.PrintGpuMemoryInfo();
-      aCtxCompat.PrintExtensions();
-    }
-  }
-
-  if (toShowWgl)
-  {
-    NativeGlContext aCtxCore("wglinfo_core_profile");
-    if (aCtxCore.CreateGlContext(NativeGlContext::ContextBits_CoreProfile))
-    {
-      aCtxCore.PrintRendererInfo();
-      aCtxCore.PrintExtensions();
-    }
-  }
-
-  if (toShowWgl)
-  {
-    NativeGlContext aCtxCore("wglinfo_gles_profile");
-    if (aCtxCore.CreateGlContext(NativeGlContext::ContextBits_GLES))
-    {
-      aCtxCore.PrintRendererInfo();
-      aCtxCore.PrintExtensions();
-    }
-  }
-
-  if (toShowWgl)
-  {
-    NativeGlContext aCtxSoft("wglinfo_ms_soft");
-    if (aCtxSoft.CreateGlContext(NativeGlContext::ContextBits_SoftProfile))
-    {
-      aCtxSoft.PrintRendererInfo();
-      aCtxSoft.PrintExtensions();
-    }
-  }
-
-  // enumerate all the formats
-  if (toPrintVisuals)
-  {
-    NativeGlContext aDummy("wglinfo_dummy");
-    if (aDummy.CreateGlContext(NativeGlContext::ContextBits_NONE))
-    {
-      aDummy.PrintVisuals(isVerbose);
-    }
-  }
-
-  if (toShowEgl)
-  {
-    EglGlContext aCtxEglGl("wglinfo_egl_gl");
-    if (aCtxEglGl.CreateGlContext(NativeGlContext::ContextBits_NONE))
-    {
-      aCtxEglGl.PrintPlatformInfo();
-      aCtxEglGl.PrintRendererInfo();
-      aCtxEglGl.PrintGpuMemoryInfo();
-      aCtxEglGl.PrintExtensions();
-      aCtxEglGl.PrintVisuals(isVerbose);
-    }
-  }
-  if (toShowEgl)
-  {
-    EglGlContext aCtxEglGles("wglinfo_egl_gles");
-    if (aCtxEglGles.CreateGlContext(NativeGlContext::ContextBits_GLES))
-    {
-      aCtxEglGles.PrintPlatformInfo();
-      aCtxEglGles.PrintRendererInfo();
-      aCtxEglGles.PrintGpuMemoryInfo();
-      aCtxEglGles.PrintExtensions();
-      aCtxEglGles.PrintVisuals(isVerbose);
-    }
-  }
-
-  return 0;
+  return true;
 }
 
-//! Returns the CPU architecture used to build the program (may not match the system).
-static const char* getArchString()
+void WglInfo::printHelp(const char* theName)
+{
+  std::string aName = theName;
+  if (aName.size() > 4 && aName.substr(aName.size() - 4) == ".exe")
+  {
+    aName = aName.substr(0, aName.size() - 4);
+  }
+  std::cout << "Usage: " << aName << " [-v] [-h] [--platform {egl|wgl}]=* [--api {gl|gles}]=*\n"
+    "               [--profile {core|compat|soft}]=*\n"
+    "  -B         Brief output, print only the basics.\n"
+    "  -v         Print visuals info in verbose form.\n"
+    "  -h         This information.\n"
+    "  --platform Platform to create OpenGL/OpenGL ES context.\n"
+    "  --api      Api to create context (OpenGL or OpenGL ES).\n"
+    "  --profile  Profile to create OpenGL context.\n"
+    "This wglinfo tool variation has been created by Kirill Gavrilov Tartynskih <kirill@sview.ru>\n";
+}
+
+template<class Platform_t>
+std::vector<BaseGlContext::ContextBits> WglInfo::printWglInfo()
+{
+  std::vector<BaseGlContext::ContextBits> anOptions;
+  if (myToShowGl && myIsCompatProfile)
+    anOptions.push_back(BaseGlContext::ContextBits_NONE);
+
+  if (myToShowGl && myIsCoreProfile)
+    anOptions.push_back(BaseGlContext::ContextBits_CoreProfile);
+
+  if (myToShowGles)
+    anOptions.push_back(BaseGlContext::ContextBits_GLES);
+
+  if (myToShowGl && myIsSoftProfile)
+    anOptions.push_back(BaseGlContext::ContextBits_SoftProfile);
+
+  std::vector<BaseGlContext::ContextBits> aSucceeded;
+  for (size_t anOptIter = 0; anOptIter < anOptions.size(); ++anOptIter)
+  {
+    const BaseGlContext::ContextBits anOpt = anOptions[anOptIter];
+
+    Platform_t aCtx("wglinfo");
+    if (!aCtx.CreateGlContext(anOpt))
+      continue;
+
+    aSucceeded.push_back(anOpt);
+    if (aSucceeded.size() == 1)
+      aCtx.PrintPlatformInfo(); // print platform once
+
+    aCtx.PrintRendererInfo();
+    aCtx.PrintGpuMemoryInfo();
+    aCtx.PrintExtensions();
+  }
+
+  return aSucceeded;
+}
+
+const char* WglInfo::getArchString()
 {
 #if defined(__amd64) || defined(__x86_64) || defined(_M_AMD64)
   return "x86_64";
@@ -139,24 +323,23 @@ static const char* getArchString()
 #elif defined(__aarch64__) && defined(__LP64__)
   return "AArch64 64-bit";
 #elif defined(__arm__) || defined(__arm64__)
-  #if defined(__ARM_ARCH_7A__)
-    return "ARMv7-A 32-bit";
-  #elif defined(__ARM_ARCH_7R__)
-    return "ARMv7-R 32-bit";
-  #elif defined(__ARM_ARCH_7M__)
-    return "ARMv7-M 32-bit";
-  #elif defined(__LP64__)
-    return "ARM 64-bit";
-  #else
-    return "ARM 32-bit";
-  #endif
+#if defined(__ARM_ARCH_7A__)
+  return "ARMv7-A 32-bit";
+#elif defined(__ARM_ARCH_7R__)
+  return "ARMv7-R 32-bit";
+#elif defined(__ARM_ARCH_7M__)
+  return "ARMv7-M 32-bit";
+#elif defined(__LP64__)
+  return "ARM 64-bit";
+#else
+  return "ARM 32-bit";
+#endif
 #else
   return "UNKNOWN";
 #endif
 }
 
-//! Print build and system information.
-static void printSystemInfo()
+void WglInfo::printSystemInfo()
 {
   std::cout << "wglinfo " << getArchString() << " (built with ";
 #if defined(__INTEL_COMPILER)
@@ -166,11 +349,11 @@ static void printSystemInfo()
 #elif defined(__clang__)
   std::cout << "Clang " << __clang_major__ << "." << __clang_minor__ << "." << __clang_patchlevel__;
 #elif defined(_MSC_VER)
-  #if _MSC_VER < 1900
-    std::cout << "MS Visual C++ " << int(_MSC_VER/100-6) << "." << int((_MSC_VER/10)-60-10*(int)(_MSC_VER/100-6));
-  #else
-    std::cout << "MS Visual C++ " << int(_MSC_VER/100-5) << "." << int((_MSC_VER/10)-50-10*(int)(_MSC_VER/100-5));
-  #endif
+#if _MSC_VER < 1900
+  std::cout << "MS Visual C++ " << int(_MSC_VER / 100 - 6) << "." << int((_MSC_VER / 10) - 60 - 10 * (int)(_MSC_VER / 100 - 6));
+#else
+  std::cout << "MS Visual C++ " << int(_MSC_VER / 100 - 5) << "." << int((_MSC_VER / 10) - 50 - 10 * (int)(_MSC_VER / 100 - 5));
+#endif
 #elif defined(__GNUC__)
   std::cout << "GCC " << __GNUC__ << "." << __GNUC_MINOR__ << "." << __GNUC_PATCHLEVEL__ << "";
 #else
@@ -184,14 +367,14 @@ static void printSystemInfo()
 #endif
   std::cout << ")";
 
-// suppress GetVersionExW is deprecated warning
+  // suppress GetVersionExW is deprecated warning
 #ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable : 4996)
 #endif
   OSVERSIONINFOEXA aVerInfo; ZeroMemory(&aVerInfo, sizeof(aVerInfo));
   aVerInfo.dwOSVersionInfoSize = sizeof(aVerInfo);
-  if (GetVersionExA((OSVERSIONINFOA* )&aVerInfo))
+  if (GetVersionExA((OSVERSIONINFOA*)&aVerInfo))
   {
     std::cout << " running on Windows " << aVerInfo.dwMajorVersion << "." << aVerInfo.dwMinorVersion << " [" << aVerInfo.dwBuildNumber << "]";
   }
@@ -200,10 +383,4 @@ static void printSystemInfo()
 #endif
 
   std::cout << "\n\n";
-}
-
-int main(int argc, const char** argv)
-{
-  printSystemInfo();
-  return actual_main(argc, argv);
 }
